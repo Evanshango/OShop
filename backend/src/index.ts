a0f84dbe565c151493568b9e0a8255babdcb4567
evanshango <evansonshango@gmail.com>
2021-03-26 01:42:47 +0300

    add docker files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,22 @@
 import mongoose from 'mongoose'
 import {app} from "./app";
-import {JWT_SECRET, MONGO_URI, PORT} from './helpers/constants'
+import {JWT_SECRET, MONGO_URI, PORT, DB_USER, DB_PASSWORD, DB_NAME} from './helpers/constants'
 
 const start = async () => {
     if (!JWT_SECRET) throw new Error('JWT_SECRET must be defined')
     if (!MONGO_URI) throw new Error('MONGO_URI must be defined')
+    if (!DB_USER) throw new Error('DB_USER must be defined')
+    if (!DB_PASSWORD) throw new Error('DB_PASSWORD must be defined')
+    if (!DB_NAME) throw new Error('DB_NAME must be defined')
     const port = PORT || 5000
 
+    let uri = MONGO_URI
+    uri = uri.replace('<username>', DB_USER)
+    uri = uri.replace('<password>', DB_PASSWORD)
+    uri = uri.replace('<dbname>', DB_NAME)
+
     try {
-        await mongoose.connect(MONGO_URI, {
+        await mongoose.connect(uri, {
                 useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
             }
         )
