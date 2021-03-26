bb53ce600a18285f4bd63e37a3a66012aaef40b6
evanshango <evansonshango@gmail.com>
2021-03-26 03:00:11 +0300

    edit next.config.js file

--- client-side/next.config.js
+++ client-side/next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL": "http://localhost:5000/api/v1"
+        // "BASE_URL": "http://localhost:5000/api/v1"
+        "BASE_URL": "http://167.71.3.10:5000/api/v1"
     }
 }
